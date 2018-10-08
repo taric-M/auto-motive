cd442e263fcfd46c66cbb2f02e107758f4ae328b
Taras <taras92.m@gmail.com>
2018-10-08 02:31:22 +0000

    Initial

--- script/script.js
+++ script/script.js
@@ -13,3 +13,26 @@
 window.onsubmit=function(e){
   window.open('order.html');
 }
+
+
+const items = document.getElementsByClassName('button_view');
+for(let i = 0; i < items.length; i++ ) {
+    items[i].onclick = el=>{
+        if(findBig()) {
+            el.currentTarget.parentElement.setAttribute("class", "gelery_item");
+            el.target.innerHTML = 'ПЕРЕГЛЯНУТИ';
+        }else{
+            el.target.parentElement.setAttribute("class", "go");
+            el.target.innerHTML = 'ЗАКРИТИ';
+        }
+    
+}
+}
+
+const findBig = () =>{
+  for(let i = 0; i < items.length; i++ ) {
+    if(items[i].innerHTML == 'ЗАКРИТИ'){
+        return true;
+    }
+}  
+}
